3e0d6bfd4b2116b776a8ddb3169743cb853493b1
Jeremy Langner <langnerj@mcmaster.ca>
2023-11-07 14:48:18 -0500

    Created fromework from scratch due to dependency errors for Cass and I

---
+++ src/app/client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>ASLingo</h1>
+        <a>Welcome to ASLingo!</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
