126efa37e465db08d28bde6719babd47f809fb44
stanreee <77902731+stanreee@users.noreply.github.com>
2023-11-13 21:18:40 -0500

    Trained model on some sign language letters

--- test/gather_data.py
+++ test/gather_data.py
@@ -3,25 +3,15 @@ import cv2
 import os
 import numpy as np
 import csv
-
-# process landmark features
-# in particular:
-#   - designate a landmark as the base landmark, calculate relative x and y coordinates on other landmarks from the base landmark
-#     this accommodates for different hand positions by having coordinates be relative to base landmark position and not camera position
-def process_features(features):
-    base_x, base_y = 0, 0
-    for feature in features:
-        if base_x == 0 and base_y == 0:
-            base_x = feature[0]
-            base_y = feature[1]
-        feature[0] = base_x - feature[0]
-        feature[1] = base_y - feature[1]
+from util import process_features
 
 # Initialize MediaPipe Hands
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
 
 cur_dir = os.getcwd()
+
+letters_to_process = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
 
 image_path = cur_dir + "/test/datasets/train/images/"
 image_gens = cur_dir + "/test/datasets/anchors/"
@@ -32,7 +22,7 @@ os.makedirs(image_gens, exist_ok=True)
 data = []
 
 for idx, ip in enumerate(os.listdir(image_path)):
-    if ip.endswith(".jpg"):
+    if ip.endswith(".jpg") and ip[0].lower() in letters_to_process:
         image = cv2.imread(image_path + ip)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
@@ -52,15 +42,13 @@ for idx, ip in enumerate(os.listdir(image_path)):
                 x, y, z = int(point.x * image.shape[1]), int(point.y * image.shape[0]), int(point.z * image.shape[1])
                 cv2.circle(image, (x, y), 5, (0, 255, 0), -1)
                 features.append([point.x, point.y])
+
+        if len(features) == 0:
+            continue
         
-        process_features(features)
+        features = process_features(features)
 
-        flattened = np.array(features).flatten()
-
-        for feature in flattened:
-            row_data.append(feature)
-
-        row = row_data
+        row = row_data + features
 
         if len(row) > 2:
             data.append(row)
---
+++ test/util.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+# process landmark features
+# in particular:
+#   - designate a landmark as the base landmark, calculate relative x and y coordinates on other landmarks from the base landmark
+#     this accommodates for different hand positions by having coordinates be relative to base landmark position and not camera position
+#   - normalize landmarks on the maximum coordinate magnitude to improve consistency
+#   referenced from https://github.com/kinivi/hand-gesture-recognition-mediapipe/tree/main
+def process_features(features):
+    base_x, base_y = 0, 0
+    for feature in features:
+        if base_x == 0 and base_y == 0:
+            base_x = feature[0]
+            base_y = feature[1]
+        feature[0] = base_x - feature[0]
+        feature[1] = base_y - feature[1]
+
+    features = np.array(features).flatten()
+
+    max_val = max(list(map(abs, features)))
+
+    def normalize(n):
+        return n / max_val
+    
+    features = list(map(normalize, features))
+
+    return features
