f43f220694b0ec165ecedaaaf0247ac8c5f00c79
stanreee <77902731+stanreee@users.noreply.github.com>
2024-01-25 14:08:01 -0500

    webcam stream to server with web sockets

--- src/app/client/src/pages/Quiz.tsx
+++ src/app/client/src/pages/Quiz.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import iQuiz from '../interfaces/iQuiz'
 import quizRaw from "../data/additionQuiz.json"
+import { io } from "socket.io-client";
+import Peer from "simple-peer";
 
 
 import '../styles/Quiz.css'
@@ -26,39 +28,93 @@ const Quiz = ({
     wrongAnswers: 0,
   })
 
+  const [stream, setStream] = useState<MediaStream>();
+  const webcamVideo = useRef<HTMLVideoElement | null>(null);
+
+  const socket = io("http://127.0.0.1:5000")
+
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext("2d");
+
+  const sendSnapshot = () => {
+    const video = webcamVideo.current;
+    ctx?.drawImage(video!, 0, 0, video!.videoWidth, video!.videoHeight, 0, 0, 300, 150);
+    let dataURL = canvas.toDataURL('image/jpeg');
+    socket.emit('stream', dataURL);
+  }
+
+  const startConnection = () => {
+    navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        height: 350,
+        width: 350,
+      },
+    })
+    .then((stream: MediaStream) => {
+      setStream(stream);
+      webcamVideo.current!.srcObject = stream;
+      sendSnapshot();
+    })
+  }
+
+  // const stopConnection = () => {
+  //   if(mediaStream){
+  //     mediaStream!.getTracks().forEach((track) => {
+  //       console.log("stopping track");
+  //       track.stop();
+  //     })
+  //     mediaStream = null;
+  //     console.log(localVideoStream.current);
+  //     // localVideoStream.current = null;
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   if(!mediaStream) startConnection();
+  //   return function cleanup() {
+  //     console.log("exiting");
+  //     stopConnection();
+  //   }
+  // }, [])
+
+  useEffect(() => {
+    startConnection();
+  }, [])
+
   const questions = quizQuestions
 
-  const { question, choices, correctAnswer } = questions[activeQuestion]
+  // const { question, choices, correctAnswer } = questions[activeQuestion]
 
-  const onClickNext = () => {
-    setSelectedAnswerIndex('')
-    setResult((prev) =>
-      selectedAnswer
-        ? {
-            ...prev,
-            score: prev.score + 1,
-            correctAnswers: prev.correctAnswers + 1,
-          }
-        : { ...prev, wrongAnswers: prev.wrongAnswers + 1 }
-    )
-    if (activeQuestion !== questions.length - 1) {
-      setActiveQuestion((prev) => prev + 1)
-    } else {
-      setActiveQuestion(0)
-      setShowResult(true)
-    }
-  }
+  // const onClickNext = () => {
+  //   setSelectedAnswerIndex('')
+  //   setResult((prev) =>
+  //     selectedAnswer
+  //       ? {
+  //           ...prev,
+  //           score: prev.score + 1,
+  //           correctAnswers: prev.correctAnswers + 1,
+  //         }
+  //       : { ...prev, wrongAnswers: prev.wrongAnswers + 1 }
+  //   )
+  //   if (activeQuestion !== questions.length - 1) {
+  //     setActiveQuestion((prev) => prev + 1)
+  //   } else {
+  //     setActiveQuestion(0)
+  //     setShowResult(true)
+  //   }
+  // }
 
-  const onAnswerSelected = (answer: string, index: number) => {
-    setSelectedAnswerIndex(index)
-    if (answer === correctAnswer) {
-      setSelectedAnswer(true)
-    } else {
-      setSelectedAnswer(false)
-    }
-  }
+  // const onAnswerSelected = (answer: string, index: number) => {
+  //   setSelectedAnswerIndex(index)
+  //   if (answer === correctAnswer) {
+  //     setSelectedAnswer(true)
+  //   } else {
+  //     setSelectedAnswer(false)
+  //   }
+  // }
 
-  const addLeadingZero = (number: number) => (number > 9 ? number : `0${number}`)
+  // const addLeadingZero = (number: number) => (number > 9 ? number : `0${number}`)
 
   return (
     <div className="quiz-container">
@@ -67,7 +123,8 @@ const Quiz = ({
           <div>
             <h3>{title} Quiz</h3>
           </div>
-          <div>
+          <video autoPlay muted playsInline ref={webcamVideo} />
+          {/* <div>
             <span className="active-question-no">{addLeadingZero(activeQuestion + 1)}</span>
             <span className="total-question">/{addLeadingZero(questions.length)}</span>
           </div>
@@ -86,7 +143,7 @@ const Quiz = ({
             <button onClick={onClickNext} disabled={selectedAnswerIndex === null}>
               {activeQuestion === questions.length - 1 ? 'Finish' : 'Next'}
             </button>
-          </div>
+          </div> */}
         </div>
       ) : (
         <div className="result">
--- src/flask_app/app.py
+++ src/flask_app/app.py
@@ -1,34 +1,16 @@
 #Import necessary libraries
 from flask import Flask, render_template, Response
+from flask_socketio import SocketIO
 import cv2
 #Initialize the Flask app
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-camera = cv2.VideoCapture(0)
-'''
-for ip camera use - rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' 
-for local webcam use cv2.VideoCapture(0)
-'''
-
-def gen_frames():  
-    while True:
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
+@socketio.on('stream')
+def stream(message):
+    print(message)
+    s = message['stream']
+    print(s)
 
 if __name__ == "__main__":
     app.run(debug=True)
