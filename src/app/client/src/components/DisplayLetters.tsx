0761db5d57c5d897b4a08984d08f92be8f4e3e1a
Cassie <cassiebaldin@outlook.com>
2024-02-01 00:24:02 -0500

    Cleaned up "Learn" alphabet, added help videos

--- src/app/client/src/components/DisplayLetters.tsx
+++ src/app/client/src/components/DisplayLetters.tsx
@@ -1,7 +1,3 @@
-// import '../styles/Exercise.css';
-import lettersResource from "../data/letters.json"
-import React from "react";
- import YouTube from "react-youtube";
 import a from "../images/learnLetters/a.png"
 import b from "../images/learnLetters/b.png"
 import c from "../images/learnLetters/c.png"
@@ -29,172 +25,225 @@ import x from "../images/learnLetters/x.png"
 import y from "../images/learnLetters/y.png"
 import z from "../images/learnLetters/z.png"
 
-// https://www.youtube.com/watch?v=0vJ7tOVhGOw
+import lettersResource from "../data/letters.json"
+import VideoClip from "./VideoClip"
 
 const DisplayLetters = () => {
 
     //DATA CALL
     const allLetters = lettersResource.allLetters;
     const letterDesc = lettersResource.lettersDesc;
+    const letterVideos = lettersResource.letterVideos;
 
     return (
     <div className="Exercise-Page">
        <div className="Exercise-content">
         <h3>
-            <img src={a} alt= "a" />
-            <br></br>
-            {allLetters[0]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={a} alt= "a" /> 
+              <h2>{allLetters[0]} </h2>
+              <VideoClip letterVideoId= {letterVideos[0]}/>
+            </div> 
             {letterDesc[0]}
+
             <div className="Section"> </div>
-            <img src={b} alt="b" />
-            <br></br>
-            {allLetters[1]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={b} alt="b" />
+              <h2>{allLetters[1]} </h2>
+              <VideoClip letterVideoId= {letterVideos[1]}/>
+            </div>
             {letterDesc[1]}
+
             <div className="Section"> </div>
-            <img src={c} alt="c" />
-            <br></br>
-            {allLetters[2]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={c} alt="c" />
+              <h2>{allLetters[2]} </h2>
+              <VideoClip letterVideoId= {letterVideos[2]}/>
+            </div>
             {letterDesc[2]}
+
             <div className="Section"> </div>
-            <img src={d} alt="d" />
-            <br></br>
-            {allLetters[3]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={d} alt="d" />
+              <h2>{allLetters[3]} </h2>
+              <VideoClip letterVideoId= {letterVideos[3]}/>
+            </div>
             {letterDesc[3]}
+
             <div className="Section"> </div>
-            <img src={e} alt="e" />
-            <br></br>
-            {allLetters[4]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={e} alt="e" />
+              <h2>{allLetters[4]} </h2>
+              <VideoClip letterVideoId= {letterVideos[4]}/>
+            </div>
             {letterDesc[4]}
+
             <div className="Section"> </div>
-            <img src={f} alt="f" />
-            <br></br>
-            {allLetters[5]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={f} alt="f" />
+              <h2>{allLetters[5]} </h2>
+              <VideoClip letterVideoId= {letterVideos[5]}/>
+            </div>
             {letterDesc[5]}
+
             <div className="Section"> </div>
-            <img src={g} alt="g" />
-            <br></br>
-            {allLetters[6]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={g} alt="g" />
+              <h2>{allLetters[6]} </h2>
+              <VideoClip letterVideoId= {letterVideos[6]}/>
+            </div>
             {letterDesc[6]}
+
             <div className="Section"> </div>
-            <img src={h} alt="h" />
-            <br></br>
-            {allLetters[7]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={h} alt="h" />
+              <h2>{allLetters[7]} </h2>
+              <VideoClip letterVideoId= {letterVideos[7]}/>
+            </div>
             {letterDesc[7]}
+
             <div className="Section"> </div>
-            <img src={i} alt="i" />
-            <br></br>
-            {allLetters[8]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={i} alt="i" />
+              <h2>{allLetters[8]} </h2>
+              <VideoClip letterVideoId= {letterVideos[8]}/>
+            </div>
             {letterDesc[8]}
+
             <div className="Section"> </div>
-            <img src={j} alt="j" />
-            <br></br>
-            {allLetters[9]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={j} alt="j" />
+              <h2>{allLetters[9]} </h2>
+              <VideoClip letterVideoId= {letterVideos[9]}/>
+            </div>
             {letterDesc[9]}
+
             <div className="Section"> </div>
-            <img src={k} alt="k" />
-            <br></br>
-            {allLetters[10]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={k} alt="k" />
+              <h2>{allLetters[10]} </h2>
+              <VideoClip letterVideoId= {letterVideos[10]}/>
+            </div>
             {letterDesc[10]}
+
             <div className="Section"> </div>
-            <img src={l} alt="l" />
-            <br></br>
-            {allLetters[11]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={l} alt="l" />
+              <h2>{allLetters[11]} </h2>
+              <VideoClip letterVideoId= {letterVideos[11]}/>
+            </div>
             {letterDesc[11]}
+
             <div className="Section"> </div>
-            <img src={m} alt="m" />
-            <br></br>
-            {allLetters[12]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={m} alt="m" />
+              <h2>{allLetters[12]} </h2>
+              <VideoClip letterVideoId= {letterVideos[12]}/>
+            </div>
             {letterDesc[12]}
+
             <div className="Section"> </div>
-            <img src={n} alt="n" />
-            <br></br>
-            {allLetters[13]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={n} alt="n" />
+              <h2>{allLetters[13]} </h2>
+              <VideoClip letterVideoId= {letterVideos[13]}/>
+            </div>
             {letterDesc[13]}
+
             <div className="Section"> </div>
-            <img src={o} alt="o" />
-            <br></br>
-            {allLetters[14]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={o} alt="o" />
+              <h2>{allLetters[14]} </h2>
+              <VideoClip letterVideoId= {letterVideos[14]}/>
+            </div>
             {letterDesc[14]}
+
             <div className="Section"> </div>
-            <img src={p} alt="p" />
-            <br></br>
-            {allLetters[15]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={p} alt="p" />
+              <h2>{allLetters[15]} </h2>
+              <VideoClip letterVideoId= {letterVideos[15]}/>
+            </div>
             {letterDesc[15]}
+
             <div className="Section"> </div>
-            <img src={q} alt="q" />
-            <br></br>
-            {allLetters[16]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={q} alt="q" />
+              <h2>{allLetters[16]} </h2>
+              <VideoClip letterVideoId= {letterVideos[16]}/>
+            </div>
             {letterDesc[16]}
+
             <div className="Section"> </div>
-            <img src={r} alt="r" />
-            <br></br>
-            {allLetters[17]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={r} alt="r" />
+              <h2>{allLetters[17]} </h2>
+              <VideoClip letterVideoId= {letterVideos[17]}/>
+            </div>
             {letterDesc[17]}
+
             <div className="Section"> </div>
-            <img src={s} alt="s" />
-            <br></br>
-            {allLetters[18]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={s} alt="s" />
+              <h2>{allLetters[18]} </h2>
+              <VideoClip letterVideoId= {letterVideos[18]}/>
+            </div>
             {letterDesc[18]}
+
             <div className="Section"> </div>
-            <img src={t} alt="t" />
-            <br></br>
-            {allLetters[19]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={t} alt="t" />
+              <h2>{allLetters[19]} </h2>
+              <VideoClip letterVideoId= {letterVideos[19]}/>
+            </div>
             {letterDesc[19]}
+
             <div className="Section"> </div>
-            <img src={u} alt="u" />
-            <br></br>
-            {allLetters[20]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={u} alt="u" />
+              <h2>{allLetters[20]} </h2>
+              <VideoClip letterVideoId= {letterVideos[20]}/>
+            </div>
             {letterDesc[20]}
+
             <div className="Section"> </div>
-            <img src={v} alt="v" />
-            <br></br>
-            {allLetters[21]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={v} alt="v" />
+              <h2>{allLetters[21]} </h2>
+              <VideoClip letterVideoId= {letterVideos[21]}/>
+            </div>
             {letterDesc[21]}
+
             <div className="Section"> </div>
-            <img src={w} alt="w" />
-            <br></br>
-            {allLetters[22]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={w} alt="w" />
+              <h2>{allLetters[22]} </h2>
+              <VideoClip letterVideoId= {letterVideos[22]}/>
+            </div>
             {letterDesc[22]}
+
             <div className="Section"> </div>
-            <img src={x} alt="x" />
-            <br></br>
-            {allLetters[23]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={x} alt="x" />
+              <h2>{allLetters[23]} </h2>
+              <VideoClip letterVideoId= {letterVideos[23]}/>
+            </div>
             {letterDesc[23]}
+
             <div className="Section"> </div>
-            <img src={y} alt="y" />
-            <br></br>
-            {allLetters[24]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={y} alt="y" />
+              <h2>{allLetters[24]} </h2>
+              <VideoClip letterVideoId= {letterVideos[24]}/>
+            </div>
             {letterDesc[24]}
+
             <div className="Section"> </div>
-            <img src={z} alt="z" />
-            <br></br>
-            {allLetters[25]} 
-            <br></br>
+            <div className = "letterAlign">
+              <img src={z} alt="z" />
+              <h2>{allLetters[25]} </h2>
+              <VideoClip letterVideoId= {letterVideos[25]}/>
+            </div>
             {letterDesc[25]}
         </h3>
       </div>
---
+++ src/app/client/src/components/VideoClip.tsx
@@ -0,0 +1,36 @@
+// from https://github.com/tjallingt/react-youtube/tree/canary?tab=readme-ov-file
+import YouTube, { YouTubeProps } from 'react-youtube';
+
+type VideoClipProps = {
+    letterVideoId: string;
+};
+
+const VideoClip = ({
+    letterVideoId
+  }: VideoClipProps) => {
+
+  const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+
+  const opts: YouTubeProps['opts'] = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 0,
+      cc_load_policy: 1,
+      controls: 1,
+    },
+  };
+
+  return (  
+    <YouTube 
+        videoId={letterVideoId}
+        opts={opts} 
+        onReady={onPlayerReady} />
+  );
+}
+
+export default VideoClip
