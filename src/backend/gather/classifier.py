0ca3049d747622b475e5690782870b4ff3596a1e
stanreee <77902731+stanreee@users.noreply.github.com>
2024-03-13 15:23:15 -0400

    Update classifier.py

--- src/backend/gather/classifier.py
+++ src/backend/gather/classifier.py
@@ -32,54 +32,61 @@ class Classifier:
         return (data, frameNum)
 
     def start(self):
-        self.cap = cv2.VideoCapture(0)
-        cv2.startWindowThread()
-        cv2.namedWindow(self.name, cv2.WINDOW_NORMAL)
-        data = []
-        frameNum = 0
-        startTime = 0
-        countdown = False
         # print("Press C to capture. If recording two hands, then a 3 second countdown will start when C is pressed before recording begins.")
-        while(input("Press c to capture. If recording two hands, then a 3 second countdown will start when c is pressed before recording begins.\n") != 'c'):
-            pass
+        # while(input("Press c to capture. If recording two hands, then a 3 second countdown will start when c is pressed before recording begins.\n") != 'c'):
+        #     pass
         while True:
-            ret, frame = self.cap.read()
+            key = input("Press c to capture. If recording two hands, then a 3 second countdown will start when c is pressed before recording begins.\n")
 
-            # if self.capturing:
-            #     frameNum += 1
-            #     data, frameNum = self.capture(frame, frameNum, data)
+            if key == 'c':
+                print("Preparing to capture...")
+                self.cap = cv2.VideoCapture(1)
+                cv2.startWindowThread()
+                cv2.namedWindow(self.name, cv2.WINDOW_NORMAL)
+                data = []
+                frameNum = 0
+                startTime = 0
+                countdown = False
+                self.capturing = True
+                print("CAPTURING 30 FRAMES")
+                while self.capturing:
+                    ret, frame = self.cap.read()
 
-            frameNum += 1
-            data, frameNum = self.capture(frame, frameNum, data)
+                    # if self.capturing:
+                    #     frameNum += 1
+                    #     data, frameNum = self.capture(frame, frameNum, data)
 
-            cv2.imshow(self.name, frame)
+                    frameNum += 1
+                    data, frameNum = self.capture(frame, frameNum, data)
 
-            if countdown:
-                deltaTime = time.time() - startTime
-                if deltaTime > 3:
-                    countdown = False
-                    self.capturing = True
-                    startTime = 0
-                    print("CAPTURING 30 FRAMES")
+                    cv2.imshow(self.name, frame)
 
-            keys = cv2.waitKey(1) & 0xFF
-            # if keys == ord('c'):
-            #     if self.num_hands > 1:
-            #         if not self.capturing and not countdown:
-            #             startTime = time.time()
-            #             countdown = True
-            #             print("CAPTURING IN 3 SECONDS")
-            #     else:
-            #         print("CAPTURING 30 FRAMES")
-            #         self.capturing = True
-            # elif keys == ord('q'):
-            #     break
-                    
-            if keys == ord('q'):
-                break
-        self.cap.release()
-        cv2.destroyAllWindows()
-        cv2.waitKey(1)
-        cv2.waitKey(1)
-        cv2.waitKey(1)
-        cv2.waitKey(1)
+                    if countdown:
+                        deltaTime = time.time() - startTime
+                        if deltaTime > 3:
+                            countdown = False
+                            self.capturing = True
+                            startTime = 0
+                            print("CAPTURING 30 FRAMES")
+
+                    keys = cv2.waitKey(1) & 0xFF
+                    # if keys == ord('c'):
+                    #     if self.num_hands > 1:
+                    #         if not self.capturing and not countdown:
+                    #             startTime = time.time()
+                    #             countdown = True
+                    #             print("CAPTURING IN 3 SECONDS")
+                    #     else:
+                    #         print("CAPTURING 30 FRAMES")
+                    #         self.capturing = True
+                    # elif keys == ord('q'):
+                    #     break
+                            
+                    if keys == ord('q'):
+                        break
+                self.cap.release()
+                cv2.destroyAllWindows()
+                cv2.waitKey(1)
+                cv2.waitKey(1)
+                cv2.waitKey(1)
+                cv2.waitKey(1)
