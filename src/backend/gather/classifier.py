1d5d410df9d5603f9167b2f9ade74ff1d046f047
Konadean <kilandrew120@gmail.com>
2024-03-13 15:07:34 -0400

    fixed issue where capture for static wasn't working

--- src/backend/gather/classifier.py
+++ src/backend/gather/classifier.py
@@ -32,19 +32,25 @@ class Classifier:
         return (data, frameNum)
 
     def start(self):
-        self.cap = cv2.VideoCapture(1)
+        self.cap = cv2.VideoCapture(0)
         cv2.startWindowThread()
+        cv2.namedWindow(self.name, cv2.WINDOW_NORMAL)
         data = []
         frameNum = 0
         startTime = 0
         countdown = False
-        print("Press C to capture. If recording two hands, then a 3 second countdown will start when C is pressed before recording begins.")
+        # print("Press C to capture. If recording two hands, then a 3 second countdown will start when C is pressed before recording begins.")
+        while(input("Press c to capture. If recording two hands, then a 3 second countdown will start when c is pressed before recording begins.\n") != 'c'):
+            pass
         while True:
             ret, frame = self.cap.read()
 
-            if self.capturing:
-                frameNum += 1
-                data, frameNum = self.capture(frame, frameNum, data)
+            # if self.capturing:
+            #     frameNum += 1
+            #     data, frameNum = self.capture(frame, frameNum, data)
+
+            frameNum += 1
+            data, frameNum = self.capture(frame, frameNum, data)
 
             cv2.imshow(self.name, frame)
 
@@ -56,16 +62,20 @@ class Classifier:
                     startTime = 0
                     print("CAPTURING 30 FRAMES")
 
-            if cv2.waitKey(10) & 0xFF == ord('c'):
-                if self.num_hands > 1:
-                    if not self.capturing and not countdown:
-                        startTime = time.time()
-                        countdown = True
-                        print("CAPTURING IN 3 SECONDS")
-                else:
-                    print("CAPTURING 30 FRAMES")
-                    self.capturing = True
-            elif cv2.waitKey(10) & 0xFF == ord('q'):
+            keys = cv2.waitKey(1) & 0xFF
+            # if keys == ord('c'):
+            #     if self.num_hands > 1:
+            #         if not self.capturing and not countdown:
+            #             startTime = time.time()
+            #             countdown = True
+            #             print("CAPTURING IN 3 SECONDS")
+            #     else:
+            #         print("CAPTURING 30 FRAMES")
+            #         self.capturing = True
+            # elif keys == ord('q'):
+            #     break
+                    
+            if keys == ord('q'):
                 break
         self.cap.release()
         cv2.destroyAllWindows()
